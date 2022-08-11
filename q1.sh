a272bf2c140f674b94c48f7ce8099835f47919ac
anu-gulhane <anuja.gulhane@gmail.com>
2022-08-11 15:13:27 +0530

    modified q1.sh

--- q1.sh
+++ q1.sh
@@ -1,8 +1,12 @@
 #! /bin/bash
 filename=$1
 n=$(wc -l < $filename)
+if(( n == 1 ))
+then cat $filename
+else
 n=$(( $n/2 ))
 awk ' { if(NR == lineno ) print $0}' lineno="$n" $filename
+fi
 #Output
 : 'anuja@anuja-VirtualBox:~/ssd/lab2Ass$ ./q1.sh textfile.txt
 Hello Hello '
